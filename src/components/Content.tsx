a5a4560acecb7e738e2cd011aa902150accf9b2f
Duy Le <nhatduy0409@gmail.com>
2023-03-02 13:25:54 +0000

    lesson3 - useCallback

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,31 @@
 import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 
-const Content = ({ name }: { name: string }) => {
+interface ContentProps {
+  name: string;
+  increase?: () => void;
+  decrease?: () => void;
+}
+
+const Content = ({ name, increase, decrease }: ContentProps) => {
   const numRender = useRef<number>(0);
 
   numRender.current = numRender.current + 1;
+
+  let content: JSX.Element | undefined = <div></div>;
+  if (typeof increase !== "undefined") {
+    content = (
+      <div>
+        <button onClick={decrease}>-</button>
+        <button onClick={increase}>+</button>
+      </div>
+    );
+  }
 
   return (
     <>
       <h3>Hi! {name}</h3>
       <h2>Full-stack Developer (Re-renders: {numRender.current})</h2>
+      {content && content}
     </>
   );
 };
--- src/lesson3/Callback.tsx
+++ src/lesson3/Callback.tsx
@@ -1,5 +1,24 @@
+import { useState, useCallback } from "react";
+import Content from "../components/Content";
+
 const Callback = () => {
-  return <>Hello useCallback with React.memo</>;
+  const [count, setCount] = useState<number>(60);
+
+  // Neu khong dung react memo thi useCallback cung khong co tac dung
+  const decrease = useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, []);
+
+  const increase = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+
+  return (
+    <div>
+      <Content name="Le Nhat Duy" increase={increase} decrease={decrease} />
+      <h1>{count}</h1>
+    </div>
+  );
 };
 
 export default Callback;
